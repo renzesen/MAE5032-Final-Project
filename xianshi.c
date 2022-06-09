4776442c1e43e9243aa897790f4602a95736c38a
renzesen <104970895+renzesen@users.noreply.github.com>
2022-06-09 20:09:32 +0800

    Add files via upload

--- xianshi.c
+++ xianshi.c
@@ -1,159 +1,254 @@
-/*
-	显示
-*/
-#include <petsc.h>
+#include <petscksp.h>
+#include <petscmath.h>
+#include <petscsys.h>
+#include <petscviewerhdf5.h>
 #include <math.h>
+#include<petscerror.h>
 #define Pai  3.14159265358979
 
 /*
       初始设定各种参数与类型
 */
 
-int main(int argc, char **argv)
+int main(int argc,char **args)
 {
-
-	PetscMPIInt		rank;
-	PetscInt		i, j, size, iteration_num, Istart, Iend, col[3];
-	PetscScalar		one = 1.0, dx = 0.01, dt = 0.00001, 
-					ro = 1.0, c = 1.0, l = 1.0, k = 1.0, a, b,
-					value[3], val, fval;
-	Vec				u_n, u_n+1, f;
-	Mat 			A;
-	
-	PetscCall(PetscInitialize(&argc,&argv,(char*)0,NULL));
-	PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD,&rank));
+  PetscInt       i, col[3], rstart, rend, nlocal, rank, iter_num = 0;
+  PetscInt       size, end, index;      
+  PetscReal      ro = 1.0, c = 1.0, k = 1.0, l = 1.0;   
+  PetscReal      a, b;   
+  PetscReal      dx= 0.00001,dt = 0.00001, t = 0.0;
+  PetscScalar    zero = 0.0, value[3], val, fval, data[3];
+  Vec               u_now, u_next, f, tem;
+  Mat               A;  
+  PetscBool      restart = PETSC_FALSE; 
+  PetscViewer   h5; 
+  PetscErrorCode ierr;
 
 /*
-      输入dx, dt, l, ro, c,k
+     初始化petsc与MPI
+*/
+
+  ierr =PetscInitialize(&argc,&args,(char*)0,NULL);if (ierr) return ierr;
+  ierr =MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
+
+/*
+     读取参数
+*/
+
+     
+   ierr =PetscOptionsGetReal(NULL,NULL,"-dx",&dx,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetReal(NULL,NULL,"-dt",&dt,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetReal(NULL,NULL,"-l",&l,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetReal(NULL,NULL,"-ro",&ro,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetReal(NULL,NULL,"-c",&c,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetReal(NULL,NULL,"-k",&k,NULL);CHKERRQ(ierr);
+   ierr =PetscOptionsGetBool(NULL,NULL,"-restart",&restart,NULL);CHKERRQ(ierr);    
+    
+
+/*
+      定义系数并输出
+*/
+
+  size                 = 1/dx+1;
+  a   	        = k*dt/(ro*c*dx*dx);
+  end                = size;
+  b                    = dt/(ro*c);
+  
+   ierr =PetscPrintf(PETSC_COMM_WORLD,"a =  %g, b = %g\n", a, b);CHKERRQ(ierr);
+   ierr =PetscPrintf(PETSC_COMM_WORLD,"dx =  %g\n", dx);CHKERRQ(ierr);
+   ierr =PetscPrintf(PETSC_COMM_WORLD,"dt =  %g\n", dt);CHKERRQ(ierr);
+   ierr =PetscPrintf(PETSC_COMM_WORLD,"restart =  %g\n", restart);CHKERRQ(ierr);
+   ierr =PetscPrintf(PETSC_COMM_WORLD,"size =  %g\n", size);CHKERRQ(ierr);
+
+/*
+      初始化  Vec u_now, u_next, f
+*/
+
+   ierr =VecCreate(PETSC_COMM_WORLD,&u_now);CHKERRQ(ierr);
+   ierr =VecSetSizes(u_now,PETSC_DECIDE,size);CHKERRQ(ierr);
+   ierr =VecSetFromOptions(u_now);CHKERRQ(ierr);
+   ierr =VecDuplicate(u_now,&u_next);CHKERRQ(ierr);
+   ierr =VecDuplicate(u_next,&f);CHKERRQ(ierr);
+
+   ierr =VecCreate(PETSC_COMM_WORLD,&tem);CHKERRQ(ierr);
+   ierr =VecSetSizes(tem, 3, PETSC_DECIDE);CHKERRQ(ierr);
+   ierr =VecSetFromOptions(tem);CHKERRQ(ierr);
+
+   ierr =VecGetOwnershipRange(u_next, &rstart, &rend);CHKERRQ(ierr);
+   ierr =VecGetLocalSize(u_next,&nlocal);CHKERRQ(ierr);
+
+/*
+       设置 Mat A
 */
 	
-	PetscOptionsGetScalar(NULL,NULL,"-dx",&dx,NULL);
-	PetscOptionsGetScalar(NULL,NULL,"-dt",&dt,NULL);
-	PetscOptionsGetScalar(NULL,NULL,"-l",&l,NULL);
-	PetscOptionsGetScalar(NULL,NULL,"-ro",&ro,NULL);
-	PetscOptionsGetScalar(NULL,NULL,"-c",&c,NULL);
-                PetscOptionsGetScalar(NULL,NULL,"-k",&k,NULL);
+   ierr =MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+   ierr =MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,size,size);CHKERRQ(ierr);
+   ierr =MatSetFromOptions(A);CHKERRQ(ierr);
+   ierr =MatMPIAIJSetPreallocation(A,3,PETSC_NULL,3,PETSC_NULL);CHKERRQ(ierr);
+   ierr =MatSetUp(A);CHKERRQ(ierr);
 
+   ierr =MatGetOwnershipRange(A,&rstart,&rend);CHKERRQ(ierr);
+
+  if (!rstart) 
+  {
+  rstart = 1;
+  i      = 0; col[0] = 0; col[1] = 1; value[0] = 1-2.0*a; value[1] = a;
+   ierr =MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+                    
+  if (rend == size)
+  {
+  rend = size-1;
+  i    = size-1; col[0] = size-2; col[1] = size-1; value[0] = a; value[1] = 1-2.0*a;
+   ierr =MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  value[0] = a; value[1] = 1-2.0*a; value[2] = a;
+  for (i=rstart; i<rend; i++)
+  {
+  col[0] = i-1; col[1] = i; col[2] = i+1;
+   ierr =MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+   ierr =MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+   ierr =MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+   ierr =MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 /*
-      定义向量与矩阵的各种系数
+      判断是否需要重读还是新建Mat 
 */
 
-	size 		  = 1/dx+1;
-	iteration_num = 1/dt+1;
-	a   	  = k*dt/(ro*c*dx*dx);
-	b 		  = dt/(ro*c);
-                PetscPrintf(PETSC_COMM_WORLD,"a =  %g, b = %g\n", a, b);
-/*
-      初始化向量
-*/
+  if(restart)
+  {   
+      ierr =PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5", FILE_MODE_READ, &h5);CHKERRQ(ierr);   
+      ierr =PetscObjectSetName((PetscObject) u_now, "explicit-vector");CHKERRQ(ierr); 
+      ierr =PetscObjectSetName((PetscObject) tem, "explicit-necess-data");CHKERRQ(ierr);  
+      ierr =VecLoad(tem, h5);CHKERRQ(ierr);    
+      ierr =VecLoad(u_now, h5);CHKERRQ(ierr); 
+      ierr =PetscViewerDestroy(&h5);CHKERRQ(ierr);   
+      index=0;   
+      ierr =VecGetValues(tem,1,&index,&dx);CHKERRQ(ierr);    
+      index=index+1;    
+      ierr =VecGetValues(tem,1,&index,&dt);CHKERRQ(ierr);    
+      index=index+1;    
+      ierr =VecGetValues(tem,1,&index,&t);CHKERRQ(ierr);   
+      index= 0;   
+  }
+  else
+  {    
 
-	PetscCall(VecCreate(PETSC_COMM_WORLD,&u_n));
-	PetscCall(VecSetSizes(u_n,PETSC_DECIDE,size));
-	PetscCall(VecSetFromOptions(u_n));
-	PetscCall(VecDuplicate(u_n,&u_n+1));
-	PetscCall(VecDuplicate(u_n+1,&f));
-
 /*
-      设置u_n
+      设置  Vec u_now
 */	
 
-	// PetscCall(VecGetOwnershipRange(u_n, &Istart, &Iend));
-	for (i=1; i<size-1; i++)
+     ierr =VecGetOwnershipRange(u_now, &rstart, &rend);CHKERRQ(ierr);
+     ierr =VecGetLocalSize(u_now,&nlocal);CHKERRQ(ierr);
+     ierr =VecSet(u_now,zero);CHKERRQ(ierr);
+    if(rank == 0)
+    {  
+                for (i=1; i<size-1; i++)
                 {
-		val = exp(i*dx);
-		PetscCall(VecSetValues(u_n,1,&i,&val,INSERT_VALUES));
-	}
-	PetscCall(VecAssemblyBegin(u_n));
-                PetscCall(VecAssemblyEnd(u_n));
-	// PetscCall(VecView(u_n,PETSC_VIEWER_STDOUT_WORLD));
+      		val = exp(i*dx);
+      		 ierr =VecSetValues(u_now,1,&i,&val,INSERT_VALUES);CHKERRQ(ierr);
+     	}
+    }
+     ierr =VecAssemblyBegin(u_now);CHKERRQ(ierr);
+     ierr =VecAssemblyEnd(u_now);CHKERRQ(ierr);
+     ierr =VecView(u_now,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  }
 
 /*
-       设置f
+       设置  Vec f
 */
-
-	// PetscCall(VecGetOwnershipRange(f, &Istart, &Iend));
+  
+   ierr =VecGetOwnershipRange(f, &rstart, &rend);CHKERRQ(ierr);
+   ierr =VecGetLocalSize(f,&nlocal);CHKERRQ(ierr);
+   ierr =VecSet(f,zero);CHKERRQ(ierr);
+  if(rank == 0)
+  {  
 	for (i=1; i<size-1; i++) 
                 {
-		val = sin(l*i*dx*Pai);
-		PetscCall(VecSetValues(f,1,&i,&val,INSERT_VALUES));
+		fval = sin(l*i*dx*Pai);
+		 ierr =VecSetValues(f,1,&i,&fval,INSERT_VALUES);CHKERRQ(ierr);
 	}
-	PetscCall(VecAssemblyBegin(f));
-                PetscCall(VecAssemblyEnd(f));
-	// PetscCall(VecView(f,PETSC_VIEWER_STDOUT_WORLD));
+  }
+   ierr =VecAssemblyBegin(f);CHKERRQ(ierr);
+   ierr =VecAssemblyEnd(f);CHKERRQ(ierr);
+   ierr =VecView(f,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-/*
-       初始化矩阵A
-*/
-	
-	PetscCall(MatCreate(PETSC_COMM_WORLD,&A));
-  	PetscCall(MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,size,size));
-  	PetscCall(MatSetFromOptions(A));
-  	PetscCall(MatMPIAIJSetPreallocation(A,3,PETSC_NULL,3,PETSC_NULL));
-	PetscCall(MatSetUp(A));
-
-	PetscCall(MatGetOwnershipRange(A,&Istart,&Iend));
-
-	if (!Istart) 
-                {
-                Istart = 1;
-                i      = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
-                PetscCall(MatSetValues(A,1,&i,2,col,value,INSERT_VALUES));
-                }
-                    
-                if (Iend == size)
-                {
-                Iend = size-1;
-                i    = size-1; col[0] = size-2; col[1] = size-1; value[0] = -1.0; value[1] = 2.0;
-                PetscCall(MatSetValues(A,1,&i,2,col,value,INSERT_VALUES));
-                }
-
-/*
-        除了两行特判之外其他都符合一行三个元素 
-*/    
-         
-                value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
-                for (i=Istart; i<Iend; i++)
-                {
-                col[0] = i-1; col[1] = i; col[2] = i+1;
-                PetscCall(MatSetValues(A,1,&i,3,col,value,INSERT_VALUES));
-                }
-
-	PetscCall(MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY));
-  	PetscCall(MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY));
-	// PetscCall(MatView(A,PETSC_VIEWER_STDOUT_WORLD));
-	
-	
 	
 /*
         显示计算 
 */  
 
-                for(i=0;i<iteration_num;i++)
-                {
-                                PetscCall(MatMult(A,u_n,u_n+1));
-                                PetscCall(VecScale(u_n+1,a));
-                                PetscCall(VecAXPY(u_n+1,b,f));
-                                PetscCall(VecAXPY(u_n+1,1.0,u_n));
-		PetscCall(VecSetValue(u_n+1,0,0.0,INSERT_VALUES));
-		PetscCall(VecSetValue(u_n+1,size-1,0.0,INSERT_VALUES));
-		PetscCall(VecAssemblyBegin(u_n+1));
-		PetscCall(VecAssemblyEnd(u_n+1));
+  while(PetscAbsReal(t)<=2.0)
+  {    
 
-		PetscCall(VecCopy(u_n+1,u_n));
-	}
-	PetscCall(VecView(u_n+1,PETSC_VIEWER_STDOUT_WORLD));
+     t += dt;   
+      ierr =MatMult(A,u_now,u_next);CHKERRQ(ierr);
+      ierr =VecAXPY(u_next,b,f);CHKERRQ(ierr);
+      ierr =VecSetValue(u_next,0,0.0,INSERT_VALUES);CHKERRQ(ierr);
+      ierr =VecSetValue(u_next,size-1,0.0,INSERT_VALUES);CHKERRQ(ierr);
+      ierr =VecAssemblyBegin(u_next);CHKERRQ(ierr);
+      ierr =VecAssemblyEnd(u_next);CHKERRQ(ierr);
 
+      ierr =VecCopy(u_next,u_now);CHKERRQ(ierr);
+  
+      ierr =VecView(u_next,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  
 
-	PetscPrintf(PETSC_COMM_WORLD,"size =  %D\n", size);
+     iter_num += 1;  
+     if((iter_num % 10) == 0)
+     {   
+     
+        data[0] = dx; data[1] = dt; data[2] = t;    
+         ierr =VecSet(tem,zero);CHKERRQ(ierr);   
+        for(index=0;index<3;index++)
+        {    
+        val = data[index];    
+         ierr =VecSetValues(tem,1,&index,&val,INSERT_VALUES);CHKERRQ(ierr);   
+        }
+         ierr =VecAssemblyBegin(tem);CHKERRQ(ierr);    
+         ierr =VecAssemblyEnd(tem);CHKERRQ(ierr);   
+
+         ierr =PetscViewerCreate(PETSC_COMM_WORLD,&h5);CHKERRQ(ierr);   
+         ierr =PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5", FILE_MODE_WRITE, &h5);CHKERRQ(ierr);    
+         ierr =PetscObjectSetName((PetscObject) u_now, "explicit-vector");CHKERRQ(ierr);    
+         ierr =PetscObjectSetName((PetscObject) tem, "explicit-necess-data");CHKERRQ(ierr);    
+         ierr =VecView(tem, h5);CHKERRQ(ierr);   
+         ierr =VecView(u_now, h5);CHKERRQ(ierr);   
+         ierr =PetscViewerDestroy(&h5);CHKERRQ(ierr);    
+     }  
+  }
+
+   ierr =VecView(u_now,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);    
 
 /*
         解构
 */
 	
-	PetscCall(VecDestroy(&u_n));
-	PetscCall(VecDestroy(&u_n+1));
-	PetscCall(VecDestroy(&f));
-	PetscCall(MatDestroy(&A));
-	PetscCall(PetscFinalize());
-	return 0;
-}
+	 ierr =VecDestroy(&u_now);CHKERRQ(ierr);
+	 ierr =VecDestroy(&u_next);CHKERRQ(ierr);
+	 ierr =VecDestroy(&f);CHKERRQ(ierr);
+                 ierr =VecDestroy(&tem);CHKERRQ(ierr);
+	 ierr =MatDestroy(&A);CHKERRQ(ierr);
+	
+                 ierr =PetscFinalize();CHKERRQ(ierr);
+                return ierr;
+}	
+	
+     
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
